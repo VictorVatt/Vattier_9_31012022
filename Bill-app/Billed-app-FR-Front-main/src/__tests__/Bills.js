178df5c3ba0a473e1cf0313f74f5be0c4e0cb703
Victor Vattier <victor.vattier35@gmail.com>
2022-02-09 15:25:11 +0100

    test de handleClickNewBill done

--- Bill-app/Billed-app-FR-Front-main/src/__tests__/Bills.js
+++ Bill-app/Billed-app-FR-Front-main/src/__tests__/Bills.js
@@ -42,21 +42,36 @@ describe("Given I am connected as an employee", () => {
   })
 })
   describe("Given I'm connected has an employee", () => {
-    describe("Given I'm an the bill page", () => {
+    describe("When I'm an the bill page", () => {
       // Test pour handleCLickIconEye
-      test('Should call the handleClickIconEye when i click on iconEye', ()=>{    
+      test('Then call the handleClickIconEye when i click on iconEye', () => {    
         document.body.innerHTML = BillsUI({ data: bills }) // J'affiche le html a partir des données mockées dans fixture/bills.js
-        const bill = new Bills({ document, onNavigate: (pathname) => document.body.innerHTML = ROUTES({ pathname })})
+        const bill = new Bills({ document, onNavigate: (pathname) => document.body.innerHTML = ROUTES({ pathname })}) // création nouvelle instance de Bills
         
         const eyesIcons = screen.getAllByTestId('icon-eye') // Selection tout les elements EYE du DOM
         expect(eyesIcons).toBeTruthy() // test si les elements EYES du DOM ne sont pas null ou undifined
         const eyeIcon1 = eyesIcons[0] // On selectionne le premeier eyeIcon (INDEX0)
         const handleClickIconEye = jest.fn(bill.handleClickIconEye(eyeIcon1))// on simule la fonction handleClickIconEye avec jest.fn sur l'icon1
         // correction dans setup jest pour utiliser jest.fn avec jquery
-        eyeIcon1.addEventListener("click", handleClickIconEye) // on ajout le listner qui call la fonction simuler au click
+        eyeIcon1.addEventListener("click", handleClickIconEye) // on ajoute le listener qui call la fonction simuler au click
         expect(eyeIcon1).toBeTruthy() // on test si l'icone est defini
         fireEvent.click(eyeIcon1) // on simule le click avec le fireEvent importé depuis jest
         expect(handleClickIconEye).toHaveBeenCalled() // test si la fonction est bien appelée
+        expect(screen.getAllByText("Justificatif")).toBeTruthy()
+      })
+
+      test("Then call the handleClickNewBill when i click on the buttonNewBill", () => {
+        document.body.innerHTML = BillsUI({ data: bills }) // J'affiche le html donc le newBill button
+        const bill = new Bills({ document, onNavigate: (pathname) => document.body.innerHTML = ROUTES({ pathname })}) // création nouvelle instance de Bills
+
+        const newBillBtn = screen.getByTestId('btn-new-bill') // recupere le btn via le data-testId
+        expect(newBillBtn).toBeTruthy() // test si le btn est défnini
+        const handleCLickNewBill = jest.fn(bill.handleClickNewBill()) // simule la fonction handleClickNewBill
+        newBillBtn.addEventListener("click", handleCLickNewBill) // ajout de l'envent listener qui call la fonction
+        fireEvent.click(newBillBtn) // on simule le click sur le btn
+        expect(handleCLickNewBill).toHaveBeenCalled() // on est si la fonction est bien call
+        expect(screen.getByText('Envoyer une note de frais')).toBeTruthy() // Test pour savoir si la page est bien affiché en testant si "Envoyer noite de frais est bien affiché"
+
       })
     })
   })
