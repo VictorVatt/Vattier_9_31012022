bbf97f74ccc44421125383c7ae64f520a18e938f
Victor Vattier <victor.vattier35@gmail.com>
2022-02-11 15:36:09 +0100

    test icone mail surbrillance et affichage du form

--- Bill-app/Billed-app-FR-Front-main/src/__tests__/NewBill.js
+++ Bill-app/Billed-app-FR-Front-main/src/__tests__/NewBill.js
@@ -2,17 +2,37 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { screen, waitFor } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import Router from "../app/Router.js"
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Then mail icon in the vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock})
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      Router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      await waitFor(() => screen.getByTestId("icon-mail"))
+      const mailIcon = screen.getByTestId('icon-mail')
+      expect(mailIcon.classList.contains("active-icon")).toBe(true)
     })
+
+    test("Then the NewBill form should be displayed", () => {
+      document.body.innerHTML = NewBillUI()
+      expect(screen.getAllByText("Envoyer une note de frais")).toBeTruthy()
+      const submitBtn = document.getElementById('btn-send-bill')
+      expect(submitBtn).toBeTruthy()
+    })
+
   })
 })
